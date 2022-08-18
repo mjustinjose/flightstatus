218f34fa025629a2e5fc0d0319afb773a264dd6a
just_ <just_@host.docker.internal>
2022-08-18 22:44:18 +0800

    ELK STACK + SPRINGBOOT APP- DOCKER COMPOSE

--- flight.status/src/main/java/com/ewd/flight/status/Application.java
+++ flight.status/src/main/java/com/ewd/flight/status/Application.java
@@ -16,7 +16,7 @@ public class Application {
 //	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args);		
 	}
 
 }
--- flight.status/src/main/java/com/ewd/flight/status/controller/FlightStatusController.java
+++ flight.status/src/main/java/com/ewd/flight/status/controller/FlightStatusController.java
@@ -1,8 +1,10 @@
 package com.ewd.flight.status.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ewd.flight.status.model.FlightStatusRequest;
 import com.ewd.flight.status.model.FlightStatusResponse;
 import com.ewd.flight.status.services.FlightStatusService;
+
 @RestController
 @RequestMapping(value = "/ewd/flight")
 public class FlightStatusController {
@@ -19,20 +22,25 @@ public class FlightStatusController {
 	@Autowired
 	private FlightStatusService flightService;
 
+	private Logger logger = (Logger) LogManager.getLogger(this.getClass());
+
 	@PostMapping(value = "/status/v1")
 	public ResponseEntity<FlightStatusResponse> flightStatusController(
 			@RequestBody FlightStatusRequest flightStatusReq) {
 		try {
-			System.out.println("flightStatusReq"+flightStatusReq.getFlightNumber());
+			System.out.println("API Inside");
+			logger.info("FlightStatus Req Data" + flightStatusReq.getFlightNumber());
 			FlightStatusResponse flightStatusResponse = flightService.retrieveFlightStatus(flightStatusReq);
-			if(flightStatusResponse!=null) {
-				System.out.println("NOT NULL");
+			if (flightStatusResponse != null) {
+				logger.info("Flight Status response");
 				return new ResponseEntity<>(flightStatusResponse, HttpStatus.OK);
 			} else {
+				logger.info("No Flight status available for the specificed flight number");
 				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 			}
 		} catch (Exception ex) {
-			System.out.println("Exceptions");
+			System.out.println("Exception");
+			logger.error("Exceptions");
 			ex.printStackTrace();
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
